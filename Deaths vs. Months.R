9fad4bdeb0b35f97cc99c5c20392e449228568fd
janviw <janviw@uw.edu>
2021-05-11 00:35:03 -0700

    graph title edit

--- Deaths vs. Months.R
+++ Deaths vs. Months.R
@@ -19,7 +19,7 @@ dates <- c('2020-01-30', '2020-02-28', '2020-03-31', '2020-04-30', '2020-05-31',
 deaths_vs_months <-  ggplot(world_data) +
   geom_bar(mapping = aes(x = date, y = total_deaths), stat = 'identity', fill = 'green') +
   labs(
-    title = "Covid Deaths over the Months",
+    title = "Covid Deaths from 01/20 to 04/21",
     x = "Months",
     y = "Total Deaths"
   )
