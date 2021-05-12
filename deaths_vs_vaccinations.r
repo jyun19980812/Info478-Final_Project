0f5779b90156be7ecac741cd632152ee275d3f1e
Neema Shokri <noshokri@uw.edu>
2021-05-11 20:26:26 -0700

    removed continents from graph

--- deaths_vs_vaccinations.r
+++ deaths_vs_vaccinations.r
@@ -9,11 +9,14 @@ deaths_and_vaccinations <- raw_covid_data %>%
   filter(location != "World") %>% 
   select(location, date, new_deaths, total_vaccinations) %>%
   na.omit() %>%
+  filter(!location %in% continents) %>%
   group_by(location) %>% 
   filter(date == max(date, na.rm = TRUE)) %>%
   ungroup(location) %>% 
   arrange(-new_deaths) %>% 
   slice_head(n = 10)
+
+continents <- c("South America", "Europe", "North America", "European Union")
 
 deaths_vs_vaccinations_plot <- ggplot(deaths_and_vaccinations) +
   geom_point(mapping = aes(x = total_vaccinations, y = new_deaths)) +
