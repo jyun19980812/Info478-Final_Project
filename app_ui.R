3138602d4a43ec9e0d6ebf727101882d67b298b7
Ellice Huang <ewhuang@uw.edu>
2021-06-08 20:41:24 -0700

    made edits and added examples of countries in avg vaccinations tab

--- app_ui.R
+++ app_ui.R
@@ -8,7 +8,7 @@ introduction_layout <- sidebarLayout(
       vaccination distribution and deaths around the world."),
     em("Data sources"),
     p(
-      "Data was collected Data was collected from ",
+      "Data was collected Data was collected from",
       strong("Johns Hopkins University (JHU)"), " and the JHU dataset is 
       maintained by a team at its ", strong("Center for Science and Engineering 
                                             (CSSE)")
@@ -157,22 +157,38 @@ page_three <- tabPanel(
 
       p(""),
       p("This plot shows the average number of vaccinations per month. 
-        Inittially, the chart just showed the average vaccinations for the
+        Initially, the chart just showed the average vaccinations for the
         world, but we decided to explore what this would look like when we split
-        it up into different income groups. The different income groups in the
-        data set were low income, lower middle income, upper middle income, and
-        high income."),
-      p("We can see that there is a positive trend, where the a
-        verage number of vaccinations is increasing over time. This applies to
+        it up into different income groups."),
+      p("The different income groups in the data set were low income, lower
+        middle income, upper middle income, and high income. The way different
+        countries are classified by income groups can be found",
+        strong(a("here", 
+                 href="https://datahelpdesk.worldbank.org/knowledgebase/articles/906519-world-bank-country-and-lending-groups")),
+        ", but here is a list of some of the countries in the different income groups:"),
+      tags$li("Low Income: Afghanistan, Burkina Faso, Congo, Ethiopia,
+              Madagascar, Rwanda, Liberia, Uganda, Yemen"),
+      tags$li("Lower Middle Income: Angola, Bangladesh, Cambodia, Egypt, India,
+              Kenya, Lesotho, Mongolia, Nicaragua, Ukraine, Vietnam"),
+      tags$li("Upper Middle Income: Argentina, Belarus, Brazil, China, Cuba,
+              Fiji, Guatemala, Jamaica, Mexico, Peru, Samoa, Thailand,
+              Turkey"),
+      tags$li("High Income: Australia, Belgium, Canada, Finland, France,
+              Italy, Kuwait, New Zealand, Portugal, Singapore, Sweden,
+              United Kingdom, United States"),
+      p(""),
+      p("We can see that there is a positive trend, where the average
+        number of vaccinations is increasing over time. This applies to
         all income groups. When we look at the low income and lower middle 
         income groups, we can see that the data about average vaccinations
         starts in January or February, but for the upper middle income and high
         income groups, the data starts in December 2020. We also noticed that
-        each groups shows that the average vaccination numbers increase more
+        each group shows that the average vaccination numbers increase more
         than the previous month except for the lower middle income group, where
-        April 2021 has a higher average of vaccinations than May 2021."),
+        April 2021 has a higher average of vaccinations than May 2021. These
+        differences could be due to how vaccines are being distributed in
+        different countries as well as how data is being reported."),
       p(""),
-      br(),
       p("Something that should be considered is that this trend may not be
         reflected when looking at different countries individually around the
         world. While this plot shows the overall average vaccination numbers for
@@ -225,7 +241,7 @@ page_four <- tabPanel(
 conclusion_panel <- tabPanel(
   "Conclusion of the Project",
   h2("Conclusion"),
-  p("Looking through the ", em("map of average death"), " we were able to 
+  p("Looking through the ", em("Map of Average Death"), " we were able to 
       observe that the proportion of average death caused from COVID-19 was 
       was higher from the countries of North America, Asia, and Europe. The plot
       enables us to discover how certain countries had striking death rate 
@@ -236,15 +252,16 @@ conclusion_panel <- tabPanel(
     decreasing numbers Covid deaths is still a pertinent issue. We hope this 
     data can highlight the importance of this issue and the need for COVID-19 
     vaccinations in order to protect vulnerable populations and decrease 
-      Covid related deaths."),
+    Covid related deaths."),
   p("Our results from the ", em("Average Vaccinations Per Month"), " 
-    visualization showed that the average number of vaccinations that higher 
-    income countries receive is a lot higher than lower income countries. Higher 
-    income countries also received the vaccines earlier than lower income 
-    countries. This indicates that there is need for better vaccine 
-      distribution."),
+    visualization showed that even though the average number of vaccinations is 
+    generally increasing over time, the average number of vaccinations that
+    higher income countries receive is a lot higher than lower income countries.
+    Higher income countries also received the vaccines earlier than lower income 
+    countries. As seen in other visualizations, this also indicates that there
+    is a need for better vaccine distribution."),
   p(
-    "Looking at the ", strong("Proportion Vaccinated vs. Proportion of New Covid Deaths"),
+    "Looking at the ", em("Proportion Vaccinated vs. Proportion of New Covid Deaths"),
     " plot we can't see any obvious trends 
     between the proportion of people fully vaccinated in a country and the 
     proportion of new deaths in that country. This is likely due to the fact 
@@ -258,10 +275,11 @@ conclusion_panel <- tabPanel(
     with ", em("58.463%"), " of their population being fully vaccinated and ",
     em("0%"), "of their population having any new deaths."
   ),
-  h2("Limitation"),
-  p("Some of the limitations of this project are that the data we used is 
-    continuously being updated, and it is self-reported. Our findings and 
-    visualizations in this project can change.")
+  h2("Limitations"),
+  p("A limitation of this project is that the data we used is continuously
+    being updated, so our findings and visualizations in this project can
+    change. Another limitation is that the data is self-reported, which means
+    that the data we use and findings may not be entirely accurate or correct.")
 )
 
 ui <- navbarPage(
